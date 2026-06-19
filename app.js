ce0db94b7c9f1dc26acfafed0b4feb21c5bfb131
iaamirRashid <aamironlyaamir@gmail.com>
2026-06-20 00:13:08 +0530

    Add root path response to Express app

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+const port =  2000;
+
+app.get("/", (req, res) => {
+  res.send("this is root path");
+})
+
 app.listen(port, () => {
   console.log(`port is listening on this port ${port}`);
 })
